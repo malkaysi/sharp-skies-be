e262742014ab5a52843a982a651dc9681d1de356
Mustafa Alkaysi <mustafaalkaysi@gmail.com>
2026-03-21 23:45:01 -0400

    Converting image to rgb array

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+from io import BytesIO
+
 from fastapi import FastAPI, File, UploadFile
+from PIL import Image
+import numpy as np
 
 app = FastAPI()
 
@@ -10,7 +14,19 @@ def health_check():
 
 @app.post("/api/upload-test")
 async def upload_test(file: UploadFile = File(...)):
+    data = await file.read()
+
+    # Open in Pillow to get image properties
+    image = Image.open(BytesIO(data))
+
+    # Convert to Numpy array to check dimensions
+    image_array = np.array(image)
+
     return {
         "filename": file.filename,
         "content_type": file.content_type,
+        "image_mode": image.mode,
+        "image_size": list(image.size),
+        "image_array_shape": list(image_array.shape),
+        "dtype": str(image_array.dtype),
     }
